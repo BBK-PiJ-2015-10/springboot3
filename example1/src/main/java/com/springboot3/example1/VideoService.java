8c637e18954cd915a306ca42d77159c05656810a
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-04-09 07:19:32 -0400

    adding basic videos

--- example1/src/main/java/com/springboot3/example1/VideoService.java
+++ example1/src/main/java/com/springboot3/example1/VideoService.java
@@ -3,17 +3,24 @@ package com.springboot3.example1;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
 public class VideoService {
 
-    private List<Video> videos = List.of(
+    private List<Video> videos = new CopyOnWriteArrayList<>(List.of(
             new Video("cat1"),
             new Video("cat2"),
             new Video("cat3")
-    );
+    ));
 
     public List<Video> getVideos() {
         return videos;
     }
+
+    public Video create(Video newVideo) {
+        videos.add(newVideo);
+        return newVideo;
+    }
+
 }
