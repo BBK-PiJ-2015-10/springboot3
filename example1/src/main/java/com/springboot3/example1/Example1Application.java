878205a4ce5cee7f5d2189eba95f22a9f33f343e
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-04-13 07:29:35 -0400

    update

---
+++ example1/src/main/java/com/springboot3/example1/Example1Application.java
@@ -0,0 +1,13 @@
+package com.springboot3.example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example1Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(Example1Application.class, args);
+    }
+
+}
